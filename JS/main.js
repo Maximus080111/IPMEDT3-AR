e141357d68a8e1c6998dc1198908deb6c86da571
MVVGamer <s1139341@student.hsleiden.nl>
2022-01-26 14:40:56 +0100

    Bugfix

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,11 @@
 let overlaybeeld = document.getElementById("overlay");
 let menu_bottom = document.getElementById("js--menu_bottom");
 
+function tekst(){
+    document.getElementById("titel").innerHTML = "dfasdfasdfasdfasdf"
+    document.getElementById("tekst").innerHTML = "dfasdfasdfasdfasdf"
+}
+
 function overlayOn() {
     overlaybeeld.style.display = "flex";
 }
