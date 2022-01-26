e07b4f79d0091e044c3e73b9937e8b57cf35a489
MVVGamer <s1139341@student.hsleiden.nl>
2022-01-26 14:59:56 +0100

    Melding wehalen wanneer je iets scant

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,11 @@
 let overlaybeeld = document.getElementById("overlay");
 let menu_bottom = document.getElementById("js--menu_bottom");
+let popup = document.getElementById("js--popup");
 
 function tekst(){
     document.getElementById("titel").innerHTML = "dfasdfasdfasdfasdf"
     document.getElementById("tekst").innerHTML = "dfasdfasdfasdfasdf"
+    popup.style.display = "none";
 }
 
 function overlayOn() {
