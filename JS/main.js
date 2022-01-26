20e7c0f0d059aebfd09704254c7a9475a0fe8189
MVVGamer <s1139341@student.hsleiden.nl>
2022-01-26 14:23:35 +0100

    patterns werken goed en info word aangepast

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 let overlaybeeld = document.getElementById("overlay");
 let menu_bottom = document.getElementById("js--menu_bottom");
 
+
+function stilte(){
+    document.getElementById("titel").innerHTML = "Hello Dolly.";
+    document.getElementById("tekst").innerHTML = "Hello Dolly.";
+}
+
 function overlayOn() {
     overlaybeeld.style.display = "flex";
 }
